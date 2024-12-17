a7f1b3473cd673d36ca81a0c7a820f666f4695b0
Keming He <keminghe.career@gmail.com>
2024-12-17 00:45:51 -0500

    refactor(vitest.setup.ts): added ci check and replace dotenv with dotenv-safe for local testing
    if process.env.ci is true, skip dotenv-safe config, else use dotenv-safe to check for missing local
    env values. also added package.json back to biome ignore list due to unexpected auto-formatting of
    the file per pnpm command execution.

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,4 @@
-import { config } from 'dotenv';
+import { config } from 'dotenv-safe';
 
-// Load environment variables from .env if available (ci-friendly).
-config();
+// If not in CI, load the .env file and check for missing variables using dotenv-safe.
+if (!process.env.CI) config();
