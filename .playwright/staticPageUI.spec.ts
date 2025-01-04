fb15639e7e995efd6734bc102a8b33597ab0a7d5
Keming He <keminghe.career@gmail.com>
2025-01-04 18:18:37 -0500

    test(.playwright/): added playwright e2e config, test, and github actions (with cache) ref socialify

---
+++ .playwright/staticPageUI.spec.ts
@@ -0,0 +1,61 @@
+import {
+  type Page,
+  type PageScreenshotOptions,
+  expect,
+  test,
+} from '@playwright/test';
+
+import {
+  AUTH_SIGN_IN_ROUTE,
+  AUTH_VERIFY_EMAIL_ROUTE,
+  HOME_ROUTE,
+} from '../constants/routeConstants';
+
+const customPageLoadTimeout = { timeout: 30000 };
+const additionalPageLoadTimeout: number = 1000;
+// As a known CI issue, allow max 0% deviation in pixel diff.
+const customDiffPixelRatio = { maxDiffPixelRatio: 0.0 };
+// Avoid screenshotting version number on pages.
+const customScreenshotOptions: PageScreenshotOptions = {
+  style: '.no-screenshot{display:none !important}',
+};
+
+test.describe('OSUSWE App UI (for static pages) is consistent with latest screenshot on...', (): void => {
+  test('homepage', async ({ page }: { page: Page }): Promise<void> => {
+    await page.goto(HOME_ROUTE, customPageLoadTimeout);
+    await page.waitForLoadState('networkidle', customPageLoadTimeout);
+    await page.waitForTimeout(additionalPageLoadTimeout);
+
+    const image = await page.screenshot(customScreenshotOptions);
+    expect(image).toMatchSnapshot(customDiffPixelRatio);
+  });
+
+  test('not found page', async ({ page }: { page: Page }): Promise<void> => {
+    await page.goto('/404', customPageLoadTimeout);
+    await page.waitForLoadState('networkidle', customPageLoadTimeout);
+    await page.waitForTimeout(additionalPageLoadTimeout);
+
+    const image = await page.screenshot(customScreenshotOptions);
+    expect(image).toMatchSnapshot(customDiffPixelRatio);
+  });
+
+  test('sign-in page', async ({ page }: { page: Page }): Promise<void> => {
+    await page.goto(AUTH_SIGN_IN_ROUTE, customPageLoadTimeout);
+    await page.waitForLoadState('networkidle', customPageLoadTimeout);
+    await page.waitForTimeout(additionalPageLoadTimeout);
+
+    const image = await page.screenshot(customScreenshotOptions);
+    expect(image).toMatchSnapshot(customDiffPixelRatio);
+  });
+
+  test('verify email page (default failure when verify code is not provided)', async ({
+    page,
+  }: { page: Page }): Promise<void> => {
+    await page.goto(AUTH_VERIFY_EMAIL_ROUTE, customPageLoadTimeout);
+    await page.waitForLoadState('networkidle', customPageLoadTimeout);
+    await page.waitForTimeout(additionalPageLoadTimeout);
+
+    const image = await page.screenshot(customScreenshotOptions);
+    expect(image).toMatchSnapshot(customDiffPixelRatio);
+  });
+});
